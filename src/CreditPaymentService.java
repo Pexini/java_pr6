92ebfef98d2c9eaf91e996f0b99e4bf23687d293
Davydov.P <davydov.works@gmail.com>
2023-07-05 14:20:41 -0400

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,13 @@
+
 public class CreditPaymentService {
-    public double calculate(int sumCred, int timeCred, double percentCred) {
+    public int calculate(int sumCred, int timeCred, double percentCred) {
         double mountlyRate = percentCred / (100 * 12);
         double x = Math.pow(1 + mountlyRate, timeCred);
 
-        double payment = 1_000_000 * (mountlyRate * x / (x - 1));
-        double finpay = (int) payment;
-        return finpay;
+        double payment = sumCred * (mountlyRate * x / (x - 1));
+        int pay = (int) payment;
+        return pay;
+
 
     }
 }
